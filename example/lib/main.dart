5f98c386731f7f2e04e5813e0a225616fa5e6519
Joshua F. Mathews <joshfeltonm@gmail.com>
2022-01-11 09:13:20 -0700

    Configures project for publishing version 0.0.1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_auth0/flutter_auth0.dart';
+import 'package:flutter_auth0_client/flutter_auth0.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
