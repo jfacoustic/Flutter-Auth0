a0b3a3723cbf76850cc6f5b11f556f1a943dbe7d
Joshua F. Mathews <joshfeltonm@gmail.com>
2022-04-07 09:31:58 -0600

    Successfully login with auth0

--- android/src/main/kotlin/com/jfacoustic/flutter_auth0_client/AuthParams.kt
+++ android/src/main/kotlin/com/jfacoustic/flutter_auth0_client/AuthParams.kt
@@ -3,7 +3,7 @@ package com.jfacoustic.flutter_auth0_client
 class AuthParams(
     val clientId: String,
     val domain: String,
+    val scheme: String,
     val scope: String? = null,
     val audience: String? = null,
-    val useEmphemeral: Boolean = false
 )
--- android/src/main/kotlin/com/jfacoustic/flutter_auth0_client/FlutterAuth0ClientPlugin.kt
+++ android/src/main/kotlin/com/jfacoustic/flutter_auth0_client/FlutterAuth0ClientPlugin.kt
@@ -2,6 +2,7 @@ package com.jfacoustic.flutter_auth0_client
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Parcel
 import android.os.Parcelable
 import androidx.annotation.NonNull
@@ -19,8 +20,10 @@ import com.auth0.android.result.Credentials
 import com.auth0.android.callback.Callback
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.PluginRegistry
+import java.lang.Exception
 import kotlin.math.log
-
+import com.google.gson.Gson;
 /** FlutterAuth0ClientPlugin */
 class FlutterAuth0ClientPlugin() : FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
@@ -29,17 +32,24 @@ class FlutterAuth0ClientPlugin() : FlutterPlugin, MethodCallHandler, ActivityAwa
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
   private lateinit var context: Context;
-  private lateinit var activity: Activity;
+  private var activity: Activity? = null;
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_auth0_client")
     channel.setMethodCallHandler(this)
     this.context = flutterPluginBinding.applicationContext
   }
-
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else if(call.method == "login") {
+      login(AuthParams(
+        clientId = call.argument<String>("clientId")!!,
+        domain = call.argument<String>("domain")!!,
+        scheme = call.argument<String>("scheme")!!,
+        scope = call.argument<String>("scope"),
+        audience = call.argument<String>("audience"),
+      ), result)
     } else {
       result.notImplemented()
     }
@@ -51,45 +61,56 @@ class FlutterAuth0ClientPlugin() : FlutterPlugin, MethodCallHandler, ActivityAwa
 
   fun login(params: AuthParams, @NonNull result: Result) {
     val auth0 = Auth0(params.clientId, params.domain);
-    var loginMethod = WebAuthProvider.login(account = auth0)
+    var loginMethod = WebAuthProvider.login(account = auth0).withScheme(params.scheme)
     if(params.scope != null) {
       loginMethod = loginMethod.withScope(params.scope);
     }
     if(params.audience != null) {
       loginMethod = loginMethod.withAudience(params.audience);
     }
-    loginMethod.start(context, object : Callback<Credentials, AuthenticationException> {
-      // Called when there is an authentication failure
-      override fun onFailure(exception: AuthenticationException) {
-        // Something went wrong!
-          result.error("AuthenticationException", exception.message, null)
-      }
+    print("About to log in")
+    try {
+      loginMethod.start(this.activity!!, object : Callback<Credentials, AuthenticationException> {
+        // Called when there is an authentication failure
+        override fun onFailure(exception: AuthenticationException) {
+          result.error("AuthenticationException", exception.message, activity)
+        }
 
-      // Called when authentication completed successfully
-      override fun onSuccess(credentials: Credentials) {
-        // Get the access token from the credentials object.
-        // This can be used to call APIs
-        result.success(mapOf(
-          "accessToken" to credentials.accessToken,
-          "idToken" to credentials.idToken,
-          "scope" to credentials.scope,
-          "recoveryCode" to credentials.recoveryCode,
-          "refreshToken" to credentials.refreshToken,
-          "tokenType" to credentials.type
-        ))
-      }
-    })
+        // Called when authentication completed successfully
+        override fun onSuccess(credentials: Credentials) {
+          val gson = Gson()
+          val data = gson.toJson(credentials)
+          result.success(data)
+//          result.success(
+////            mapOf(
+////              "accessToken" to credentials.accessToken,
+////              "idToken" to credentials.idToken,
+////              "scope" to credentials.scope,
+////              "recoveryCode" to credentials.recoveryCode,
+////              "refreshToken" to credentials.refreshToken,
+////              "tokenType" to credentials.type
+////            )
+//          )
+        }
+      })
+    } catch (e: Exception) {
+      result.error("AuthenticationException", e.message, activity)
+    }
   }
 
   override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+      this.activity = binding.activity
   }
 
   override fun onDetachedFromActivityForConfigChanges() {
+    this.activity = null
   }
 
   override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    this.activity = binding.activity
   }
 
   override fun onDetachedFromActivity() {
+    this.activity = null
   }
 }
